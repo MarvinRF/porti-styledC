3f96e2ae9334bfe10c84931d31130f6c2840303b
MarvinRF <mamavivi@gmail.com>
2024-04-08 00:29:17 -0300

    bug fix add 2 sections

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/marvinrf.png" />
+    <Titulo fontSize={20}>Marvin Rocha</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,17 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <>
+    <section>
+      <Titulo fontSize={16}>Sobre mim</Titulo>
+      <Paragrafo tipo="secundario">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi beatae
+        vitae amet ad laborum esse! Maiores perspiciatis exercitationem commodi.
+        Obcaecati dolorem officia optio sit in vero vel quidem minus ab?
+      </Paragrafo>
+    </section>
+  </>
+)
+
+export default Sobre
