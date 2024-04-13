05e9e6b73aa6d185ce0a0d928fdafab7cb2e36d7
MarvinRF <mamavivi@gmail.com>
2024-04-12 21:58:32 -0300

    'Changes color scheme and adds Github stats section to Sobre page'

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -3,6 +3,6 @@ import { Props } from './index'
 
 export const Paragrafo = styled.p<Props>`
   font-size: 14px;
-  color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
+  color: ${(props) => (props.tipo === 'principal' ? '#919191' : '#ffff')};
   line-height: 22px;
 `
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './style'
 
 const Sobre = () => (
   <>
@@ -10,6 +11,10 @@ const Sobre = () => (
         vitae amet ad laborum esse! Maiores perspiciatis exercitationem commodi.
         Obcaecati dolorem officia optio sit in vero vel quidem minus ab?
       </Paragrafo>
+      <GithubSecao>
+        <img src="https://github-readme-stats.vercel.app/api?username=MarvinRF&show_icons=true&theme=prussian&include_all_commits=true&count_private=true" />
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MarvinRF&layout=compact&langs_count=7&theme=prussian" />
+      </GithubSecao>
     </section>
   </>
 )
